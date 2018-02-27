4594a0a76da2553368f05f5a07229d14d3ef27e9
Oscar Sebastián <osebastianv@gmail.com>
2018-02-27 11:29:03 +0100

    added mixins and articles list

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,62 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  // entry point: archivo que lee webpack para construir el grafo de dependencias
+  entry: path.join(__dirname, "src", "entry.js"),
 
-    // entry point: archivo que lee webpack para construir el grafo de dependencias
-    entry: path.join(__dirname, 'src', 'entry.js'),
+  // output: carpeta en la que quiero que webpack me deje el código generado
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
 
-    // output: carpeta en la que quiero que webpack me deje el código generado
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-
-    // module loaders
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader?url=false', 'sass-loader']
-            }, {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: path.join(__dirname, 'node_modules')
-            }, {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                use: [
-                    'file-loader?name=[name].[ext]&useRelativePath=true',
-                    'image-webpack-loader'
-                ]
-            }, {
-                test: /assets.[^img]/,
-                use: 'file-loader?name=[name].[ext]&useRelativePath=true'
-            }
+  // module loaders
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader?url=false", "sass-loader"]
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: path.join(__dirname, "node_modules")
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          "file-loader?name=[name].[ext]&useRelativePath=true",
+          "image-webpack-loader"
         ]
-    },
+      },
+      {
+        test: /assets.[^img]/,
+        use: "file-loader?name=[name].[ext]&useRelativePath=true"
+      }
+    ]
+  },
 
-    // plugins que estamos utilizando
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html'),
-            minify: {
-                collapseWhitespace: true
-            }
-        }),
-    ],
+  // plugins que estamos utilizando
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+      minify: {
+        collapseWhitespace: true
+      }
+    })
+  ],
 
-    // dev server configuration
-    devServer: {
-        open: true, // abre el navegador por defecto
-        port: 3000, // puerto del servidor web
-        overlay: true, // muestra los errores en pantalla
-        hot: true,
-        contentBase: path.join(__dirname, 'src'),
-        watchContentBase: true
-    }
-
+  // dev server configuration
+  devServer: {
+    open: true, // abre el navegador por defecto
+    port: 3000, // puerto del servidor web
+    overlay: true, // muestra los errores en pantalla
+    hot: true,
+    contentBase: path.join(__dirname, "src"),
+    watchContentBase: true
+  }
 };
